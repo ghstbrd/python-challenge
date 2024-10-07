0c13e64911fc9101282c6ba3e0da95286302d058
ghstbrd <jared.ddriver@gmail.com>
2024-10-07 18:36:29 -0500

    Update pybank_main.py

--- pybank_main.py
+++ pybank_main.py
@@ -14,9 +14,9 @@ import os
 
 # Files to load and output (update with correct file paths)
 
-file_to_load = os.path.join("Desktop", "Classwork", "Homework", "Module 3 HW", "Starter_Code", "PyBank", "Resources", "budget_data.csv")  # Input file path
+file_to_load = os.path.join("Starter_Code", "PyBank", "Resources", "budget_data.csv")  # Input file path
 
-file_to_output = os.path.join("analysis", "budget_analysis.txt")  # Output file path
+file_to_output = os.path.join("Starter_Code", "Pybank", "pybank_analysis.txt")  # Output file path
 
 
 
